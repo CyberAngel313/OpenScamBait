5599f6ffc81b75d2e7450d455cac867ba2ad002a
Phroggy <thatguyyoublocked@outlook.com>
2023-03-22 19:10:30 +0800

    sADAD

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ from os import system
 system("color 0a")
 
 def BDbuild():
-    Popen("pyinstaller --onefile toolkit/Backdoor/backdoor.py")
+    Popen("pyinstaller --onefile --onedir toolkit/Backdoor/backdoor.py")
 
 if argv[1] == "backdoor":
     BDbuild()
